7682acf24afe1adec55105a6a255b508443b9a2c
olga-supranovich <olga.bogush87@gmail.com>
2024-03-03 20:07:55 +0100

    hw 5.2.2 set baseUrl in config

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    projectId: "oigaea"
+    projectId: "oigaea",
+    baseUrl: 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/',
   },
 });
